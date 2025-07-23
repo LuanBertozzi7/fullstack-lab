0c71ba4024df263faa18cac7cf3907163a280e20
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-22 23:43:18 -0400

    add about scope in js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const pontuacaoUsuario = 1000;
-// Operação ternária -> (condição) ? 'true' : 'false'
-const nivelUsuario = pontuacaoUsuario => 1000 ? "Usuario VIP" : "Usuario Normal;"
+// const pontuacaoUsuario = 1000;
+// // Operação ternária -> (condição) ? 'true' : 'false'
+// const nivelUsuario = pontuacaoUsuario => 1000 ? "Usuario VIP" : "Usuario Normal;"
+
+
+// let tem escopo de bloco { ... bloco }
+
+function HelloWorld() {
+var helloworld = 'hello world';
+// as funções tem um escopo especial, não permite que as var/let sejam acessadas de fora
+// do escopo da propria função, como um COFRE
+  return helloworld;  
+}
+
+// hosting
+console.log(sobrenome);
+// sobrenome foi declarado lá emcima, pela ungine do js, mas sem seu valor
+var sobrenome = 'Bertozzi'
+
