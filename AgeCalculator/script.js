b5f4bef35f5e7d2766119ab2d5e300bb79b67f6a
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-23 13:52:12 -0400

    feat: age calculator with js

---
+++ AgeCalculator/script.js
@@ -0,0 +1,10 @@
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const anoNascimento = document.getElementById('AnoDeNascimento').value;
+  const anoAtual = new Date().getFullYear();
+  const idade = anoAtual - anoNascimento;
+  document.querySelector('.resultado').textContent = 
+  idade >= 0 ? `Você tem ${idade} anos` : `Ano Inválido!`;
+  console.warn(idade);
+});
