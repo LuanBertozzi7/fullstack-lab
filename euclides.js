60887849b2430853ce3e85bbc043b87599585211
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-23 17:07:09 -0400

    feat: added MMC and MDC using euclides algorithm

---
+++ euclides.js
@@ -0,0 +1,24 @@
+//  function to calculate MDC using the euclides algorithm 
+function calculateMDC(a, b) {
+  while(b) {
+    const rest = a % b;
+    a = b;
+    b = rest;
+  }
+  return a;
+}
+// function to calculate MMC
+function calculateMMC(a, b) {
+  if(a === 0 || b === 0) {
+    return 0;
+  }
+  return (a*b) / calculateMDC(a, b);
+}
+
+// const firstNumber = prompt("first Number:");
+// const secondNumber= prompt("second Number");
+
+// const MDC = calculateMDC(firstNumber, secondNumber);
+// const MMC = calculateMMC(firstNumber, secondNumber);
+  
+// console.log(`MDC:${MDC} MMC:${MMC}`);
