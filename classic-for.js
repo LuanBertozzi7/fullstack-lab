d97d1dbc3036b3eabd41eb398a2d474379ad8bc6
LuanBertozzi7 <LuanBertozzi7@hotmail.com>
2025-07-24 12:28:52 -0400

    feat: for in

--- classic-for.js
+++ classic-for.js
@@ -1,7 +1,20 @@
-// for ( let i = 0; i <= 5; i++ ) {
-//     console.log(`Linha ${i}`);
+const fruts = ["pear", "watermelon", "litter"];
+
+// for (i = 0; i < fruts.length; i++) {
+//   console.log(fruts[i])
 // }
 
+for (let i in fruts) {
+  console.log(fruts[i]);
+} // for in
 
-// for in
+const cars = {
+  model: "onix",
+  engine: "v12",
+  color: "red",
+  id: 482208
+}
 
+for (let i in cars){
+  console.log(cars)
+}
