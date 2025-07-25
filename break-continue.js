1905d035fd3f6086a8bd260ef191b13221f18e2c
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-25 11:17:11 -0400

    send break and continue for loops

---
+++ break-continue.js
@@ -0,0 +1,17 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (numero of numeros) {
+
+  // if (numero === 2 || numero === 10) {
+  //   continue;
+  // }
+  
+  if (numero === 7){
+    break;
+  }
+  console.log(numero);
+  
+}
+
+
+// adicionar numeros pro array numeros, e verificar
