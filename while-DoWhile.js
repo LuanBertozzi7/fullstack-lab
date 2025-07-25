0e8bdce4c4ddc9c3a3686155ffa0cbd6c96f6cbe
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-25 11:00:13 -0400

    feat: while and doWhile

--- while-DoWhile.js
+++ while-DoWhile.js
@@ -1,8 +1,25 @@
-let i = 0;
+// let i = 0;
+// while(i <= 10) {
+//   console.log(i);
+//   i++
+// };
 
-while(i <= 10) {
-  console.log(i);
-  i++
+
+// // RANDOM NUMBER
+function RandomNumber(min, max) {
+  const r = Math.random() * (max- min) + min;
+  return Math.floor(r);
 };
 
+// let rand = RandomNumber(1, 50);
+// console.log(rand);
+// while (rand !== 10) {
+//     rand = RandomNumber(1, 50);
+//     console.log(rand)
+// }
+// console.log('numero é 10')
 
+do {
+  rand = RandomNumber(1, 50);
+  console.log(rand);
+} while(rand !== 10);
