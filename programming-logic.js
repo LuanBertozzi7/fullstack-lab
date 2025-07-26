28fad3e0cc0ca18267e0910480eb2bb528d4c160
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-26 14:21:39 -0400

    add JsDoc, and simple maintenance in the code

--- programming-logic.js
+++ programming-logic.js
@@ -1,54 +1,84 @@
+// FUNCTION THAT RETURNS THE LARGER NUMBER BETWEEN TWO VARIABLES
+const firstNumber = 102;
+const secondNumber = 9;
 
-/* FUNÇÂO QUE RETORNA O MAIOR NUMERO ENTRE DUAS VARIAVEIS */
-// const firstNumber = 102;
-// const secondNumber = 9;
+/**
+ * 
+ * @param {number} firstNumber 
+ * @param {number} secondNumber 
+ * @returns {string} // check the numbers, and returns a string about them
+ */
 
-// function biggerBetweenTwo(firstNumber, secondNumber) {
-
-//   if(firstNumber > secondNumber){
-//     return `o número ${firstNumber} é maior`;
-//   } else if (secondNumber > firstNumber) {
-//     return `o número ${secondNumber} é maior`;
-//   } else if (firstNumber === secondNumber) {
-//     return `ambos os numeros são iguais`;
-//   }
-//   return result;  
-// };
-
-
-// console.log(biggerBetweenTwo(firstNumber, secondNumber));
-
-/* FUNÇÂO QUE VERIFICA SE UMA IMAGEM(RECEBENDO DOIS ARGUMENTOS, ALTURA E LARGURA, E RETORNA SE 
-ESTÁ NO MODO PAISAGEM  */
-// function isLandscape(width, height) {
+function biggerBetweenTwo(firstNumber, secondNumber) {
   
-//   if( width > height ) {
-//     return `your image, already in landscape`
-//   } else if ( width < height ) {
-//     return `your image, already in portrait` 
-//   } else {
-//     return `your image is square`
-//   }
-// }
+  // Check if the first number is greater than the second
+  if (firstNumber > secondNumber) {
+    return `The number ${firstNumber} is bigger`;
+  } 
+  // Check if the second number is greater than the first
+  else if (secondNumber > firstNumber) {
+    return `The number ${secondNumber} is bigger`;
+  } 
+  // If both numbers are equal
+  else if (firstNumber === secondNumber) {
+    return `Both numbers are equal`;
+  }
+  // No need for this return, but kept for safety
+  return null;  
+}
 
+console.log(biggerBetweenTwo(firstNumber, secondNumber));
+
+/**
+ * @param {number} width 
+ * @param {number} height 
+ * @returns {string} // FUNCTION THAT CHECKS IF AN IMAGE (GIVEN WIDTH AND HEIGHT) IS IN LANDSCAPE MODE
+ */
+function isLandscape(width, height) {
+  // Width greater than height means landscape
+  if (width > height) {
+    return `Your image is already in landscape`;
+  } 
+  // Width less than height means portrait
+  else if (width < height) {
+    return `Your image is already in portrait`;
+  } 
+  // Width equals height means square
+  else {
+    return `Your image is square`;
+  }
+}
+
+
+// Loop from 0 to 100 and save the last number in a variable
+let forNumber;
 for (let i = 0; i <= 100; i++) {
-  let forNumber;
-  forNumber = i
+  forNumber = i;
 }
 
+
+// FUNCTION THAT RETURNS "Fizz", "Buzz", "FizzBuzz" OR THE NUMBER BASED ON DIVISIBILITY
 function FizzNumber(Usernumber) {
-  if(typeof Usernumber !== "number") {
-    return `is NaN`
+  // Check if input is a number
+  if (typeof Usernumber !== "number") {
+    return `is NaN`;
   }
-  if(Usernumber % 3 === 0){
-    return `Fizz`
-  } else if (Usernumber % 5 === 0){
-    return `Buzz`
-  } else if (Usernumber % 3 === 0 && Usernumber % 5 === 0) {
-    return `FizzBuzz`
-  } else {
-    return Usernumber
+  // Check if divisible by both 3 and 5 first
+  if (Usernumber % 3 === 0 && Usernumber % 5 === 0) {
+    return `FizzBuzz`;
+  }
+  // Check if divisible by 3
+  else if (Usernumber % 3 === 0) {
+    return `Fizz`;
+  }
+  // Check if divisible by 5
+  else if (Usernumber % 5 === 0) {
+    return `Buzz`;
+  }
+  // If none of the above, return the number itself
+  else {
+    return Usernumber;
   }
 }
 
-console.log(FizzNumber(forNumber))
+console.log(FizzNumber(forNumber));
