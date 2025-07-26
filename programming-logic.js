931b7577a561370c3f9597367947fee50707d23c
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-25 20:13:35 -0400

    fix: resolved about a return unwated

--- programming-logic.js
+++ programming-logic.js
@@ -1,17 +1,23 @@
-const firstNumber = 1;
-const secondNumber = 9;
 
-function biggerBetweenTwo(firstNumber, secondNumber) {
-let result;
-  if(firstNumber > secondNumber){
-    result = console.log(`o número ${firstNumber} é maior`);
-  } else if (secondNumber > firstNumber) {
-    result = console.log(`o número ${secondNumber} é maior`);
-  } else if (firstNumber === secondNumber) {
-    result = console.log(`ambos os numeros são iguais`);
-  }
-  return result;  
-};
+/* FUNÇÂO QUE RETORNA O MAIOR NUMERO ENTRE DUAS VARIAVEIS */
+// const firstNumber = 102;
+// const secondNumber = 9;
+
+// function biggerBetweenTwo(firstNumber, secondNumber) {
+
+//   if(firstNumber > secondNumber){
+//     return `o número ${firstNumber} é maior`;
+//   } else if (secondNumber > firstNumber) {
+//     return `o número ${secondNumber} é maior`;
+//   } else if (firstNumber === secondNumber) {
+//     return `ambos os numeros são iguais`;
+//   }
+//   return result;  
+// };
 
 
-console.log(biggerBetweenTwo(firstNumber, secondNumber));
+// console.log(biggerBetweenTwo(firstNumber, secondNumber));
+
+/* FUNÇÂO QUE VERIFICA SE UMA IMAGEM(RECEBENDO DOIS ARGUMENTOS, ALTURA E LARGURA, E RETORNA SE 
+ESTÁ NO MODO PAISAGEM  */
+
