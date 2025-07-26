3ad9811c45befa28dd4df10b98853f5095028fd2
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-26 14:26:05 -0400

    simple maintenance

--- while-DoWhile.js
+++ while-DoWhile.js
@@ -1,23 +1,14 @@
-// let i = 0;
-// while(i <= 10) {
-//   console.log(i);
-//   i++
-// };
+let i = 0;
+while(i <= 10) {
+  console.log(i);
+  i++
+};
 
-
-// // RANDOM NUMBER
+// RANDOM NUMBER
 function RandomNumber(min, max) {
   const r = Math.random() * (max- min) + min;
   return Math.floor(r);
 };
-
-// let rand = RandomNumber(1, 50);
-// console.log(rand);
-// while (rand !== 10) {
-//     rand = RandomNumber(1, 50);
-//     console.log(rand)
-// }
-// console.log('numero é 10')
 
 do {
   rand = RandomNumber(1, 50);
