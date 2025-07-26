8ac3e0430c1559907624a6db3920cc6f8f40f5af
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-26 13:46:55 -0400

    refactor: simple maintenance

--- classic-for.js
+++ classic-for.js
@@ -7,21 +7,29 @@ const cars = {
   id: 482208
 };
 
-// CLASSIC FOR
-// for (i = 0; i < fruts.length; i++) {
-//   console.log(fruts[i])
-// }
-// FOR IN
-// for (let i in fruts) {
-//   console.log(fruts[i]);
-// } // for in
-// FOR OF
-// fruts.forEach(function(elemento, indice){
-//   console.log(elemento, indice);
-// });
-// OTHERS
-// for (let key in cars) {
-//   console.log(`Itens: ${key}: ${cars[key]}`)
-// }; 
-// for (let value of fruts) {
-//   console.log(value); }
+// --- Loop Examples in JavaScript ---
+
+// 1. Classic FOR: iterates over array using index
+for (let i = 0; i < fruts.length; i++) {
+  console.log(`Classic FOR: ${fruts[i]}`);
+}
+
+// 2. FOR IN: iterates over array indices
+for (let i in fruts) {
+  console.log(`FOR IN (array): ${fruts[i]}`);
+}
+
+// 3. FOR IN with object: iterates over object keys
+for (let key in cars) {
+  console.log(`FOR IN (object): ${key}: ${cars[key]}`);
+}
+
+// 4. FOR OF: iterates over array values
+for (let value of fruts) {
+  console.log(`FOR OF: ${value}`);
+}
+
+// 5. forEach: executes a function for each array element and index
+fruts.forEach(function(element, index){
+  console.log(`forEach: ${element}, index: ${index}`);
+});
