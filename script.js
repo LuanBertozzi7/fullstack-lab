104707cdd9470b566181de8dc325b3d925ca898a
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-26 14:24:42 -0400

    refactor: a simple maintenance

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-// const pontuacaoUsuario = 1000;
-// // Operação ternária -> (condição) ? 'true' : 'false'
-// const nivelUsuario = pontuacaoUsuario => 1000 ? "Usuario VIP" : "Usuario Normal;"
-
-
-// let tem escopo de bloco { ... bloco }
+// ternary operation -> (condition) ? 'true' : 'false'
+const userScore = 1000;
+const userLevel = userScore => 1000 ? "VIP user" : "normal user;"
 
 function HelloWorld() {
 var helloworld = 'hello world';
-// as funções tem um escopo especial, não permite que as var/let sejam acessadas de fora
-// do escopo da propria função, como um COFRE
   return helloworld;  
 }
-
-// hosting
-console.log(sobrenome);
-// sobrenome foi declarado lá emcima, pela ungine do js, mas sem seu valor
-var sobrenome = 'Bertozzi'
 
