ddf24d8b3b3e2435b75629b02ba78c9957dbc676
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-07-28 18:31:00 -0400

    feat: function-parameters.js && funcition.js
    added two example files to repo

---
+++ function-parameters.js
@@ -0,0 +1,16 @@
+// function sum(a, b = 0) {
+//   // b = b || 0 default value
+//   console.log(a + b);
+
+// }
+
+
+
+
+function newPerson(name, lastname) {
+  return { name, lastname };
+};
+
+const firstPerson = newPerson('Luan', 'Bertozzi');
+console.log(firstPerson)
+
---
+++ function.js
@@ -0,0 +1,16 @@
+// hoisting
+// first-class object
+const nome = function() {
+  console.log('Luan Bertozzi')
+}
+// arrow function 
+const arrow = () => {
+}
+// function in object
+const obj = {
+  say: function() {
+    console.log("hello world")
+  }
+}
+obj.say();
+
